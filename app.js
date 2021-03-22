8b945c4bdf6a854495f8656136d33dca385f75b7
crgvrr <cluiguevarra@gmail.com>
2021-03-22 15:13:23 +0800

    Third commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const questions = document.querySelectorAll('.faq_section');
+
+questions.forEach(function (question) {
+  const btn = question.querySelector('.question');
+  btn.addEventListener('click', function () {
+    questions.forEach(function (item) {
+      if (item !== question) item.classList.remove('show-text');
+    });
+    question.classList.toggle('show-text');
+  });
+});
